ee1d6c4216545ee546e31f515f9b9ff41b0ede87
Devin Flood <devin.p.flood@gmail.com>
2025-10-02 15:34:30 -0700

    Add resumes table with delete functionality

--- src/app/models/resume/resume.model.ts
+++ src/app/models/resume/resume.model.ts
@@ -1,36 +1,165 @@
+import { Profile, ProfileDTO } from '@models/profile';
+
 import { BaseModel } from '../base.model';
 
 export class Resume extends BaseModel {
-    name?: string;
+    fileName?: string;
+    profile?: Profile;
+    sections!: SectionType[];
+    summary?: string;
+    skills?: string[];
+    education?: Education[];
+    experience?: Experience[];
+    description?: string;
+    templateSettings?: TemplateSettings;
 
     constructor(props: Partial<Resume>) {
         super(props);
-        Object.assign(this, props);
+        Object.assign(this, {
+            ...props,
+            created: this.created,
+            updated: this.updated,
+            sections: props.sections ?? defaultSections
+        });
     }
 
     static normalize(record: ResumeDTO): Resume {
         return new Resume({
             id: record.resumeId,
-            name: record.name
+            profile: record.profile ?
+                Profile.normalize(record.profile)
+                : undefined,
+            sections: record.sections,
+            summary: record.summary,
+            skills: record.skills,
+            experience: record.experience,
+            education: record.education,
+            fileName: record.fileName,
+            templateSettings: record.templateSettings,
+            description: record.description,
+            created: record.created,
+            updated: record.updated
         });
     }
 
     static serialize(resume: Resume): ResumeDTO {
         return {
             resumeId: resume.id,
-            name: resume.name
+            profile: resume.profile
+                ? Profile.serialize(resume.profile)
+                : undefined,
+            sections: resume.sections,
+            summary: resume.summary,
+            skills: resume.skills,
+            experience: resume.experience,
+            education: resume.education,
+            fileName: resume.fileName,
+            templateSettings: resume.templateSettings,
+            description: resume.description
         };
     }
 
-    static getMockDTO(isNew = false): ResumeDTO {
+    static getMockDTO(resumeId?: string): ResumeDTO {
         return {
-            resumeId: isNew ? undefined : '1',
-            name: 'Devin Flood'
+            resumeId,
+            updated: '8/27/2025',
+            fileName: `Mock_Resume_${resumeId}`,
+            templateSettings: {
+                templateName: 'default',
+                fontFamily: 'helvet'
+            },
+            profile: Profile.getMockDTO('1'),
+            description: 'Angular Focused',
+            summary: 'Senior Software Engineer with 6+ years of experience in developing enterprise-grade solutions within the wealth management sector.',
+            skills: [
+                'Angular, React',
+                'Java, Ruby, Go',
+                'jenkins, CI/CD',
+                'Code Review',
+                'UI/UX Design',
+                'Product/Technical Requirements'
+            ],
+            experience: [
+                {
+                    employer: 'MyVest',
+                    location: 'San Francisco, CA',
+                    startDate: '01/2019',
+                    endDate: '07/2025',
+                    title: 'Senior Software Engineer',
+                    description: 'Fintech company delivering portfolio management at scale',
+                    bulletPoints: ['First', 'Second', 'Third']
+                }
+            ],
+            education: [
+                {
+                    name: 'Villanova University',
+                    location: 'Villanova, PA',
+                    type: 'B.S. Chemical Engineering',
+                    completionDate: '05/2010'
+                },
+                {
+                    name: 'App Academy',
+                    location: 'San Francisco, CA',
+                    type: 'Software Engineering Program',
+                    completionDate: '11/2018'
+                }
+            ]
         }
     }
 }
 
 export interface ResumeDTO {
     resumeId?: string;
+    fileName?: string;
+    templateSettings?: TemplateSettings;
+    profile?: ProfileDTO;
+    skills?: string[];
+    sections?: SectionType[];
+    experience?: Experience[];
+    education?: Education[];
+    summary?: string;
+    description?: string;
+    updated?: string;
+    created?: string;
+}
+
+export enum SectionType {
+    Experience = 'Experience',
+    Education = 'Education',
+    Skills = 'Skills',
+    Summary = 'Summary'
+}
+
+const defaultSections = [
+    SectionType.Summary,
+    SectionType.Skills,
+    SectionType.Experience,
+    SectionType.Education
+];
+
+export interface Section {
+    type?: SectionType,
+    value?: Experience[] | Education [] | string[] | string
+};
+
+interface Experience {
+    employer?: string;
+    location?: string;
+    title?: string;
+    startDate?: string;
+    endDate?: string;
+    description?: string;
+    bulletPoints?: string[];
+};
+
+interface Education {
     name?: string;
+    location?: string;
+    type?: string;
+    completionDate?: string;
+}
+
+interface TemplateSettings {
+    templateName: 'default';
+    fontFamily: 'helvet' | 'times'
 }
---
+++ src/app/pages/resume-builder/components/resumes-table/resumes-table.component.ts
@@ -0,0 +1,145 @@
+import { SelectionModel } from '@angular/cdk/collections';
+import {
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    Component,
+    effect,
+    input,
+    output,
+    ViewChild
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+    MatCheckboxChange,
+    MatCheckboxModule
+} from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import {
+    MatPaginator,
+    MatPaginatorModule,
+    PageEvent
+} from '@angular/material/paginator';
+
+import { Resume } from '@models/resume';
+
+@Component({
+    selector: 'resumes-table',
+    templateUrl: './resumes-table.component.html',
+    styleUrl: './resumes-table.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        MatTableModule,
+        MatPaginatorModule,
+        MatCheckboxModule,
+        MatButtonModule,
+        MatIconModule,
+        MatTooltipModule
+    ]
+})
+export class ResumesTableComponent implements AfterViewInit {
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+    resumes = input<Resume[]>([]);
+    newResume = output();
+    editResume = output<Resume>();
+    viewResume = output<Resume>();
+    copyResume = output<Resume>();
+    downloadResume = output<string>();
+    deleteResumes = output<string[]>();
+    columns = ['select', 'fileName',  'description', 'updated'];
+    dataSource = new MatTableDataSource<Resume>([]);
+    selectionModel = new SelectionModel<Resume>(true, []);
+    visibleRows: Resume[] = [];
+    pageIndex = 0;
+    pageSize = 10;
+    pageSizeOptions = [10, 15, 20];
+
+    get selectedResumes(): Resume[] {
+        return this.selectionModel.selected;
+    }
+
+    get selectedResume(): Resume {
+        return this.selectionModel.selected[0];
+    }
+
+    get allSelected(): boolean {
+        return (
+            this.selectionModel.hasValue() &&
+            this.selectionModel.selected.length === this.visibleRows.length
+        );
+    }
+
+    get selectAllIndeterminate(): boolean {
+        return this.selectionModel.hasValue() && !this.allSelected
+    }
+
+    get singleSelection(): boolean {
+        return this.selectionModel.selected.length === 1;
+    }
+
+    constructor() {
+        effect(() => {
+            const resumes = this.resumes();
+            if (resumes.length > 20) {
+                this.pageSizeOptions.push(resumes.length);
+            }
+            this.dataSource.data = resumes;
+        });
+    }
+
+    ngAfterViewInit(): void {
+        this.dataSource.paginator = this.paginator;
+        this.setVisibleRows(this.pageIndex, this.pageSize);
+        this.paginator.page.subscribe(({ pageIndex, pageSize }: PageEvent) => {
+            this.setVisibleRows(pageIndex, pageSize);
+            this.selectionModel.clear();
+        })
+    }
+
+    onSelectAll({ checked }: MatCheckboxChange): void {
+        console.log('onSelectAll', checked);
+        if (checked) {
+            console.log('visibleRows', this.visibleRows)
+            this.selectionModel.setSelection(...this.visibleRows);
+        } else {
+            console.log('clear')
+            this.selectionModel.clear();
+        }
+    }
+
+    onSelectResume(resume: Resume): void {
+        if (this.selectionModel.isSelected(resume)) {
+            this.selectionModel.deselect(resume);
+        } else {
+            this.selectionModel.select(resume);
+        }
+    }
+
+    onDeleteResumes(resumes: Resume[]): void {
+        this.deleteResumes.emit(this.getIDs(resumes));
+        this.selectionModel.clear();
+    }
+
+    onDownloadResume({ id }: Resume): void {
+        if (id) {
+            this.downloadResume.emit(id);
+        }
+    }
+
+    private setVisibleRows(pageIndex: number, pageSize: number): void {
+        const start = pageIndex * pageSize;
+        const end = start + pageSize;
+        this.visibleRows = this.dataSource.filteredData.slice(start, end);
+    }
+
+    private getIDs(resumes: Resume[]): string[] {
+        return resumes.reduce((IDs: string[], resume: Resume) => {
+            if (resume.id) {
+                IDs.push(resume.id)
+            }
+
+            return IDs
+        }, []);
+    }
+}
