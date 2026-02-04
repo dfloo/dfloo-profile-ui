a0fc9f00b5c8ad90be5ec98aa27a7c52e8fb6f32
Devin Flood <devin.p.flood@gmail.com>
2026-02-04 15:14:25 -0800

    Set active tab on init

--- src/app/pages/job-search/job-search.component.ts
+++ src/app/pages/job-search/job-search.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterLinkWithHref, RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import {
+    ActivatedRoute,
+    RouterLinkWithHref,
+    RouterOutlet
+} from '@angular/router';
 import { MatTabLink, MatTabNav, MatTabNavPanel } from '@angular/material/tabs';
 
 @Component({
@@ -18,5 +22,14 @@ export class JobSearchComponent {
         { path: 'resume-builder', label: 'Resume Builder' },
         { path: 'application-tracker', label: 'Application Tracker' },
     ];
-    activeRoute = this.childRoutes[0];
+    activePath = signal(this.childRoutes[0].path);
+
+    private route = inject(ActivatedRoute);
+
+    ngOnInit(): void {
+        const child = this.route.snapshot.firstChild;
+        const path = child?.routeConfig?.path || 'resume-builder';
+
+        this.activePath.set(path);
+    }
 }
